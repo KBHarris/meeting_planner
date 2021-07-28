74ab7c77c5111ce5cf89db593765dbc4e6e4a60c
KBHarris <kylebradlyharris@gmail.com>
2021-07-28 17:01:44 -0500

    added meetings detail view and template

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html", {
+        "num_meetings": Meeting.objects.count()
+    })
