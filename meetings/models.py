8c37711b9d71cb65ec38d01e7fbae3bd98b6f664
KBHarris <kylebradlyharris@gmail.com>
2021-07-28 15:30:35 -0500

    Added forignkey to meetings class

--- meetings/models.py
+++ meetings/models.py
@@ -3,15 +3,6 @@ from datetime import time
 
 # Create your models here.
 
-class Meeting(models.Model):
-    title = models.CharField(max_length=200)
-    date = models.DateField()
-    start_time = models.TimeField(default=time(9))
-    duration = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.title} at {self.start_time} on {self.date}"
-
 class Room(models.Model):
     name = models.CharField(max_length=50)
     floor_number = models.IntegerField()
@@ -19,4 +10,16 @@ class Room(models.Model):
 
     def __str__(self):
         return f"{self.name}: Room {self.room_number} on Floor {self.floor_number}"
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
+
+
 
