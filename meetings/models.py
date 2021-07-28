b3ffc9b14475783a7cca3ce91a6f0234acaa0037
KBHarris <kylebradlyharris@gmail.com>
2021-07-28 15:24:48 -0500

    Added Room class model

--- meetings/models.py
+++ meetings/models.py
@@ -9,3 +9,14 @@ class Meeting(models.Model):
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
 
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor_number = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}: Room {self.room_number} on Floor {self.floor_number}"
+
