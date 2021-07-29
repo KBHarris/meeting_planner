97b69a2889dc1c271394971d96a79ed053c7b39d
KBHarris <kylebradlyharris@gmail.com>
2021-07-29 10:49:28 -0500

    modifies url mappings for meeting app

--- meeting_planner/urls.py
+++ meeting_planner/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls.conf import include
 from website.views import welcome
 from meetings.views import detail, room_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name='home'),
-    path('meetings/<int:id>', detail, name='detail'),
-    path('rooms', room_list, name='rooms' )
+    path('meetings/', include('meetings.urls'))
 ]
---
+++ meetings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+urlpatterns = [
+    path('<int:id>', views.detail, name='detail'),
+    path('rooms', views.room_list, name='rooms'),
+]
